9243a5f2c725c6a0e3bd85e965a08254158fb585
Calum Price - Work <calumprice.dev@gmail.com>
2025-12-11 08:37:48 +0000

    first version of guessing game

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Number Guessing Game!");
+    println!("The Secret Number is between 1 and 100");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Guess a number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please enter a valid number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Low"),
+            Ordering::Greater => println!("Too High"),
+            Ordering::Equal => {
+                println!("You Guessed Correctly");
+                break;
+            }
+            
+        }
+    }
 }
