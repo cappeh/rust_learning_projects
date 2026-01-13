58043ced201bd40d15f818efe621e8580e5230ad
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-13 13:55:16 +0000

    run returns Result<(), Box<dyn Error>> and main handles whether an error is returned

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,14 +12,16 @@ fn main() {
     println!("Searching for `{}`", config.query);
     println!("In File: {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("should have been able to read the file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("with text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
