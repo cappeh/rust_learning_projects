42dbef6eab4427733d36517545988c20c29cfeb5
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-13 15:57:18 +0000

    started library crate implementation with the search function. Brought into main's scope

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,8 @@
 use std::{env, fs, process};
 use std::error::Error;
 
+use minigrep::search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -8,9 +10,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for `{}`", config.query);
-    println!("In File: {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
@@ -20,7 +19,11 @@ fn main() {
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("with text:\n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
