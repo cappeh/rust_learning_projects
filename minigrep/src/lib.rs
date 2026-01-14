828fc5e65d619b193493fdb4e17a84877abefff8
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-14 10:54:27 +0000

    implemented logic in search to return a vec or matching lines and pass the test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,13 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -10,10 +18,10 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:\
-        safe, fast, productive.\
-        pick three.";
+Rust:
+safe, fast, productive.
+pick three.";
 
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
