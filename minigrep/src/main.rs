9020a3be5df65e078b9721a564e4357cde5b1d44
Calum Price <calumprice.dev@gmail.com>
2026-01-24 18:14:16 +0000

    changed the program to make use of iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,9 @@ use std::error::Error;
 use minigrep::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -40,20 +40,28 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didnt get a query string"),
+        };
 
-        if args.contains(&"--case-sensitive".to_string()) {
-            ignore_case = false;
-        } else if args.contains(&"--ignore-case".to_string()) {
-           ignore_case = true;
-        }
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didnt get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        // if args.contains(&"--case-sensitive".to_string()) {
+        //     ignore_case = false;
+        // } else if args.contains(&"--ignore-case".to_string()) {
+        //    ignore_case = true;
+        // }
 
         Ok(Config { query, file_path, ignore_case })
     }
