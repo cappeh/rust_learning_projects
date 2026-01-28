c37a4a2e9e1c24f81fbb345e60612c0d48443736
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-28 11:43:21 +0000

    using a map to store specific folder names for extensions and cleaned up some logic

--- file_organizer/src/main.rs
+++ file_organizer/src/main.rs
@@ -1,6 +1,20 @@
-use std::{fs, path::Path};
+use std::{collections::HashMap, fs, path::Path};
+
+fn extensions() -> HashMap<&'static str, &'static str> {
+    let mut ext_map = HashMap::new();
+    ext_map.insert("jpg", "images");
+    ext_map.insert("png", "images");
+    ext_map.insert("pdf", "docs");
+    ext_map.insert("txt", "docs");
+    ext_map.insert("zip", "dart_bundles");
+    ext_map.insert("pcapng", "packet_captures");
+    ext_map.insert("pcap", "packet_captures");
+    ext_map.insert("json", "json");
+    ext_map
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> { 
+    let ext_map = extensions();
     let dir = Path::new("/Users/calupric/Downloads/");
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
@@ -10,22 +24,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             continue;
         }
 
-        let ext = match path.extension().and_then(|e| e.to_str()) {
-            Some(ext) => ext.to_lowercase(),
-            None => "no_extension".to_string(),
-        };
+        let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("no_ext").to_lowercase();
 
-        let dest_dir = dir.join(&ext);
+        if let Some(folder) = ext_map.get(ext.as_str()) {
+            let dst_folder = dir.join(folder);
+            if !dst_folder.exists() {
+                fs::create_dir(&dst_folder)?;
+            }
 
-        if !dest_dir.exists() {
-            fs::create_dir(&dest_dir)?;
+            if let Some(filename) = path.file_name() {
+                let dst_path = dst_folder.join(filename);
+                fs::rename(&path, &dst_path)?;
+            }
         }
-
-        let file_name = path.file_name().unwrap();
-        let dest_path = dest_dir.join(file_name);
-
-        fs::rename(&path, &dest_path)?;
-
     }
 
     Ok(())
