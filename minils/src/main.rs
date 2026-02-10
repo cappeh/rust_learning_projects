8d1457eadd90ed26f31c5e9f9894bacba320d6aa
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-10 08:14:47 +0000

    simple example of reading a directory given as an arg

--- minils/src/main.rs
+++ minils/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+fn main() -> std::io::Result<()> {
+
+    let args: Vec<String> = env::args().collect();
+
+    let dir_arg = &args[1];
+
+    for entry in fs::read_dir(dir_arg)? {
+        let dir = entry?;
+        println!("{:?}", dir.path());
+    }
+
+    Ok(())
 }
